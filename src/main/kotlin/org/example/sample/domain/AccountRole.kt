1c46c80bea9039f02e2eb64dc32eeca520d3d963
jayking <mojunseo49@naver.com>
2024-05-09 09:39:34 +0900

    feat: Authority 추가

--- src/main/kotlin/org/example/sample/domain/AccountRole.kt
+++ src/main/kotlin/org/example/sample/domain/AccountRole.kt
@@ -1,11 +1,23 @@
 package org.example.sample.domain
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 enum class AccountRole(val label: String) {
     @field: JsonProperty("user")
     USER("사용자"),
 
     @field: JsonProperty("admin")
-    ADMIN("관리자")
+    ADMIN("관리자");
+
+    fun getGrantAuthorities(): List<SimpleGrantedAuthority> {
+        return when (this) {
+            USER -> listOf(SimpleGrantedAuthority(Permission.CAN_READ.value))
+            ADMIN -> listOf(
+                SimpleGrantedAuthority(Permission.CAN_READ.value),
+                SimpleGrantedAuthority(Permission.CAN_WRITE.value),
+                SimpleGrantedAuthority(Permission.CAN_DELETE.value)
+            )
+        }
+    }
 }
