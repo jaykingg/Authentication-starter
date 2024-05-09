a6812a7841d4bb0650a0c193d27a58283889544c
jayking <mojunseo49@naver.com>
2024-05-09 10:52:25 +0900

    feat: swagger url filter 추가

--- src/main/kotlin/org/example/sample/config/SecurityConfig.kt
+++ src/main/kotlin/org/example/sample/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.example.sample.core.JwtProvider
 import org.example.sample.core.JwtUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -12,22 +13,28 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        val authenticationManager = JwtProvider(JwtUtil())
+        val jwtAuthenticationManager = JwtProvider(JwtUtil())
         return http
             .csrf { csrf -> csrf.disable() }
-            .authorizeExchange { authorize ->
-                authorize
+            .authorizeExchange { exchange ->
+                exchange
                     .pathMatchers("/api/auth/register").permitAll()
                     .pathMatchers("/api/auth/token").permitAll()
+                    .pathMatchers("/swagger-ui/**").permitAll()
+                    .pathMatchers("/swagger**").permitAll()
+                    .pathMatchers("/webjars/**").permitAll()
+                    .pathMatchers("/api-docs/**").permitAll()
+                    .pathMatchers("/swagger.html").permitAll()
                     .anyExchange().authenticated()
             }
-            .authenticationManager(authenticationManager)
+            .authenticationManager(jwtAuthenticationManager)
             .build()
     }
 }
