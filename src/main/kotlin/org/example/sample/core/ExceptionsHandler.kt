0bbdcf4d973768f2ffa54f7476b717bf8300e195
jayking <mojunseo49@naver.com>
2024-05-09 11:44:41 +0900

    fix: ExceptionsHandler ResponseStatus 설정

--- src/main/kotlin/org/example/sample/core/ExceptionsHandler.kt
+++ src/main/kotlin/org/example/sample/core/ExceptionsHandler.kt
@@ -4,6 +4,7 @@ import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.bind.support.WebExchangeBindException
 import org.springframework.web.server.ResponseStatusException
@@ -14,6 +15,7 @@ import org.springframework.web.server.ServerWebInputException
 @Order(-2)
 class ExceptionsHandler {
     @ExceptionHandler(ResponseStatusException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleResponseStatusExceptions(ex: ResponseStatusException): ErrorResponse {
         return ErrorResponse(
             code = ex.statusCode.value(),
@@ -23,6 +25,7 @@ class ExceptionsHandler {
     }
 
     @ExceptionHandler(WebExchangeBindException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleValidationExceptions(ex: WebExchangeBindException): ErrorResponse {
         val errorMessage = ex.bindingResult.fieldErrors.joinToString { it.defaultMessage ?: "" }
         return ErrorResponse(
@@ -33,6 +36,7 @@ class ExceptionsHandler {
     }
 
     @ExceptionHandler(ServerWebInputException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleValidationExceptions(ex: ServerWebInputException): ErrorResponse {
         return ErrorResponse(
             code = HttpStatus.BAD_REQUEST.value(),
@@ -42,6 +46,7 @@ class ExceptionsHandler {
     }
 
     @ExceptionHandler(BadRequestException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleBadRequest(ex: BadRequestException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.BAD_REQUEST.value(),
@@ -52,6 +57,7 @@ class ExceptionsHandler {
     }
 
     @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleNotFound(ex: NotFoundException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.NOT_FOUND.value(),
@@ -62,6 +68,7 @@ class ExceptionsHandler {
     }
 
     @ExceptionHandler(InternalServerException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleInternalError(ex: InternalServerException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.INTERNAL_SERVER_ERROR.value(),
